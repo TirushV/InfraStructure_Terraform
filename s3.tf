823a6c258a879223643ca687b42d583a14cdf633
TirushV <84138572+TirushV@users.noreply.github.com>
2021-11-03 13:42:57 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 # stores terraform state file in s3
 terraform {
   backend "s3" {
-    bucket = "my-terraform-bucket"
+    bucket = "terraformstatefile2"
     key    = "Myfolder/file/terraform.tfstate"
     region = "ap-south-1"
   }
