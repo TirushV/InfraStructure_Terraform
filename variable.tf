9b781ecaa06209fed4e135a1bc96fbaaa9063d97
TirushV <84138572+TirushV@users.noreply.github.com>
2021-11-03 11:57:24 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,25 @@
+# VPC Variables
+variable "region" {
+  default       = "ap-south-1"
+  description   = "AWS Region"
+  type          = string
+}
+
+variable "vpc-cidr" {
+  default       = "10.0.0.0/16"
+  description   = "VPC CIDR Block"
+  type          = string
+}
+
+variable "public-subnet-1-cidr" {
+  default       = "10.0.1.0/24"
+  description   = "Public Subnet 1 CIDR Block"
+  type          = string
+}
+
+
+variable "public-subnet-2-cidr" {
+  default       = "10.0.2.0/24"
+  description   = "Public Subnet 2 CIDR Block"
+  type          = string
+}
