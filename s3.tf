1d80c8e151c6aac5587fd360519c542001328caf
TirushV <84138572+TirushV@users.noreply.github.com>
2021-11-03 13:41:02 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,13 +1,4 @@
-# creates a private bucket
-resource "aws_s3_bucket" "b" {
-    bucket = "my-terraform-bucket"
-    acl    = "private"
-
-    tags   = {
-        Name = "My bucket"
-        Environment = "Prod"
-    }
-}# stores terraform state file in s3
+# stores terraform state file in s3
 terraform {
   backend "s3" {
     bucket = "my-terraform-bucket"
